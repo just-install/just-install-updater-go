66b58a5535d54a5fa59e27af1d4813629fd5ee53
Patrick G <geek1011@outlook.com>
2018-04-12 12:19:25 -0400

    Added common functions for extractors

---
+++ jiup/rules/helpers/common.go
@@ -0,0 +1,62 @@
+package helpers
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// GetURL gets a url. The client is optional.
+func GetURL(c *http.Client, url string, headers map[string]string, acceptableStatuses []int) ([]byte, int, bool, error) {
+	if c == nil {
+		c = &http.Client{
+			Timeout: time.Duration(time.Second * 10),
+		}
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, 0, false, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, 0, false, err
+	}
+	defer resp.Body.Close()
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, 0, false, err
+	}
+
+	a := false
+	for _, s := range acceptableStatuses {
+		if s == resp.StatusCode {
+			a = true
+			break
+		}
+	}
+
+	return buf, resp.StatusCode, a, nil
+}
+
+// GetDoc gets a goquery doc from a url.
+func GetDoc(c *http.Client, url string, headers map[string]string, acceptableStatuses []int) (*goquery.Document, error) {
+	buf, s, a, err := GetURL(c, url, headers, acceptableStatuses)
+	if err != nil {
+		return nil, err
+	}
+	if !a {
+		return nil, fmt.Errorf("unexpected response status: %d", s)
+	}
+	return goquery.NewDocumentFromReader(bytes.NewReader(buf))
+}
